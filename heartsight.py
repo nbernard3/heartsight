c4441b5dd5fca7ae050ad4e90821f6892605466b
NicoBernard <nicolas.bernard.supaero@gmail.com>
2020-05-03 15:42:20 +0200

    added utility function

--- heartsight.py
+++ heartsight.py
@@ -1,9 +1,23 @@
 
 from datetime import datetime
 
+
+def dlib_rectangle_to_xywh(rect):
+    # take a bounding predicted by dlib and convert it
+    # to the format (x, y, w, h) as we would normally do
+    # with OpenCV
+    x = rect.left()
+    y = rect.top()
+    w = rect.right() - x
+    h = rect.bottom() - y
+    # return a tuple of (x, y, w, h)
+    return (x, y, w, h)
+
+
 def timestamp(name):
     return "%s_%s" \
         % (datetime.now().strftime('%Y-%m-%dT%H-%M-%S'), name)
+
 
 if __name__ == '__main__':
     pass
